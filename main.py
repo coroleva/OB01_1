66148c2107e9d182f9412cc7e949431e610ca32b
Юлия <coroleva.yu@gmail.com>
2024-09-13 21:19:19 +0300

    add new code in main

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ class Warrior():
         self.endurence = endurence
         self.hair_color = hair_color
 
-    def sleeep(self):
+    def sleep(self):
         print(f'{self.name} is sleeping')
         self.endurence +=2
+        self.info(self.endurence)
 
     def eat(self):
         print(f'{self.name} is eating')
         self.power+=1
+        self.info(self.power)
 
     def attack(self):
         print(f'{self.name} is attacking')
         self.endurence -=6
+        self.info(self.endurence)
 
     def walk(self):
         print(f'{self.name} is walking')
         self.endurence -=1
-    def info(self):
-        print(f'имя воина {self.name}, \n'
-              f'сила воина {self.power}, \n'
-              f'выносливость воина {self.endurence}, \n'
-              f'цвет волос воина {self.hair_color}')
+        self.info(self.endurence)
+
+    def info(self,sign):
+        if sign == self.name:
+            print(f'имя воина {sign}')
+        if sign == self.power:
+            print(f'сила воина {sign}')
+        if sign == self.endurence:
+            print(f'выносливость воина {sign}')
+        if sign == self.hair_color:
+            print(f'цвет волос воина {sign}')
+
 
 war1 = Warrior('Степа', 10, 10, 'black')
-print(war1.info())
+
+war1.sleep()
+war1.eat()
+war1.attack()
+war1.walk()
+
+
+
